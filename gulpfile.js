ae9d28dee7daed03ea51125633712b3e3a4ee20d
BartekG <bartek9873@gmail.com>
2016-11-06 23:12:49 +0100

    Confugured gulp-concat + bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,66 @@
 var gulp = require("gulp");
 var connect = require("gulp-connect");
 var open = require("gulp-open");
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
 //Starts Local development server
 var config = {
     port: 9000,
     baseURL: "http://localhost",
-    paths:{
+    paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        css: ['./node_modules/bootstrap/dist/css/bootstrap.min.css',
+            './node_modules/bootstrap/dist/css/bootstrap-theme.min.css'],
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 };
 
-gulp.task('connect', function(){
+gulp.task('connect', function () {
     connect.server({
-        root: ['dist'],
-        port: config.port,
-        base: config.baseURL,
-        livereload: true
+            root: ['dist'],
+            port: config.port,
+            base: config.baseURL,
+            livereload: true
         }
     )
 });
 
-gulp.task('open',['connect'],function(){
+gulp.task('open', ['connect'], function () {
     gulp.src('dist/index.html')
         .pipe(open({uri: config.baseURL + ':' + config.port + '/'}));
 });
 
-gulp.task('html', function(){
-   gulp.src(config.paths.html)
-       .pipe(gulp.dest(config.paths.dist))
-       .pipe(connect.reload());
+gulp.task('html', function () {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
-gulp.watch(config.paths.html, ['html']);
+gulp.task('css', function () {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'))
+        .pipe(connect.reload());
 });
 
-gulp.task('default', [ 'html', 'open', 'watch' ]);
+gulp.task('js', function () {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'css', 'js', 'open', 'watch',]);
---
+++ src/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Bartek on 06.11.2016.
+ */
+$ = jQuery = require('jQuery');
+var App = console.log("hello world");
+
+module.exports = App;
