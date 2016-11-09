53a104479e5d6cec22bc48e21bf2fcc25604b15a
BartekG <bartek9873@gmail.com>
2016-11-09 23:15:39 +0100

    Refactor

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -7,7 +7,7 @@ var React = require('react');
 var AuthorAPI = require('../../api/authorApi');
 var AuthorList = require('./authorList');
 
-var Authors = React.createClass({
+var AuthorsPage = React.createClass({
     getInitialState: function () {
         return {
             authors: []
@@ -28,4 +28,4 @@ var Authors = React.createClass({
     }
 });
 
-module.exports = Authors;
+module.exports = AuthorsPage;
