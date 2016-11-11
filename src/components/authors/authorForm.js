457de3697143f7fde8700953b3dda3e4e948def0
BartekG <bartek9873@gmail.com>
2016-11-11 16:29:41 +0100

    Created validation form

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -4,7 +4,12 @@ var React = require('react');
 var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({
-
+    propTypes:{
+      author: React.PropTypes.object.isRequired,
+        onSave: React.PropTypes.func.isRequired,
+        onChange: React.PropTypes.func.isRequired,
+        errors: React.PropTypes.object
+    },
     render: function () {
         return (
             <form>
@@ -14,6 +19,7 @@ var AuthorForm = React.createClass({
                     label="First Name"
                     value={this.props.author.firstName}
                     onChange={this.props.onChange}
+                    error={this.props.errors.firstName}
                 />
 
                 <Input
@@ -21,6 +27,7 @@ var AuthorForm = React.createClass({
                     label="Last Name"
                     value={this.props.author.lastName}
                     onChange={this.props.onChange}
+                    error={this.props.errors.lastName}
                 />
 
                 <input type="submit" value="Save" className="btn btn-default" onClick={this.props.onSave}/>
--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -2,27 +2,60 @@ var React = require('react');
 var Router = require('react-router');
 var AuthorForm = require('./authorForm');
 var AuthorAPI = require('../../api/authorApi');
+var toastr = require('toastr');
 var ManageAuthorPage = React.createClass({
     getInitialState: function () {
         return {
-            author: { id: '', firstName: '', lastName: '' }
+            author: {id: '', firstName: '', lastName: ''},
+            errors: {},
+            dirty: false
         };
     },
 
-    mixins:[
+    mixins: [
         Router.Navigation
-        ],
+    ],
+
+    statics: {
+        willTransitionFrom: function (transition, component) {
+            if (component.state.dirty && !confirm("Leave without saving?")) {
+                transition.abort();
+            }
+        }
+    },
 
     setAuthorState: function (event) {
         var field = event.target.name;
+        this.setState({dirty: true});
         var value = event.target.value;
         this.state.author[field] = value;
-        return this.setState({author:this.state.author});
+        return this.setState({author: this.state.author});
     },
 
+    authorFormIsValid: function () {
+        var formIsValid = true;
+        this.state.errors = {}
+        if (this.state.author.firstName.length < 3) {
+            this.state.errors.firstName = 'First name must be at least 3 char.'
+            formIsValid = false;
+        }
+        if (this.state.author.lastName.length < 3) {
+            this.state.errors.lastName = 'Last name must be at least 3 char.'
+            formIsValid = false;
+        }
+        this.setState({errors: this.state.errors});
+        return formIsValid;
+    },
     saveAuthor: function (event) {
-      event.preventDefault();
+        event.preventDefault();
+
+        if (!this.authorFormIsValid()) {
+            return;
+        }
+
         AuthorAPI.saveAuthor(this.state.author);
+        this.setState({dirty:false});
+        toastr.success('Author Saved');
         this.transitionTo('authors');
     },
 
@@ -32,6 +65,7 @@ var ManageAuthorPage = React.createClass({
                 author={this.state.author}
                 onChange={this.setAuthorState}
                 onSave={this.saveAuthor}
+                errors={this.state.errors}
             />
         );
     },
